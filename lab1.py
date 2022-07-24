1551af97e5e0cf1f9cbbcfc250e70de1979b23ad
AsilGa23-meet <Asillimar@gmail.com>
2022-07-24 23:47:33 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -5,4 +5,14 @@ def create_youtube_video(title,description):
 def like(video):
 	if "likes" in video():
 		video["likes"]+=1
+def dislike(video):
+	if "dislikes" in video():
+		video["dislikes"]+=1
+def add_comment(video,username,comment_text):
+	comment = { "username" : "comment_text"}
+	return comment
 
+create_youtube_video(potato,a_vid_of_a_potato):
+for i in range(495)
+	like(potato):
+	
